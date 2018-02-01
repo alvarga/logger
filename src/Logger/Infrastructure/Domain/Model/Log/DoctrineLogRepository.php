05d2464a7553af93519862da1335bda21003d953
Alen Varga <perokc@gmail.com>
2018-02-01 10:30:44 +0100

    Independed connection for writing log to DB even on throwing exception

--- src/Logger/Infrastructure/Domain/Model/Log/DoctrineLogRepository.php
+++ src/Logger/Infrastructure/Domain/Model/Log/DoctrineLogRepository.php
@@ -7,24 +7,37 @@ use Doctrine\ORM\EntityRepository;
 
 class DoctrineLogRepository extends EntityRepository implements LogRepositoryInterface
 {
+    private $setIdTable;
     /**
-     * 
+     *
      * {@inheritDoc}
      * @see \Logger\Domain\Model\Log\LogRepositoryInterface::add()
      */
     public function add(Log $log)
     {
         $this->getEntityManager()->persist($log);
+        try{
+            $this->getEntityManager()->flush($log);
+            $this->getEntityManager()->commit();
+        }catch (\Throwable $e) {
+            $this->getEntityManager()->close();
+            $this->getEntityManager()->rollBack();
+        }
     }
     
     /**
-     * 
+     *
      * {@inheritDoc}
      * @see \Logger\Domain\Model\Log\LogRepositoryInterface::remove()
      */
     public function remove(Log $log)
     {
         $this->getEntityManager()->remove($log);
+    }
+    
+    public function setIdTable($idTable)
+    {
+        $this->idTable = $idTable;
     }
 }
 
